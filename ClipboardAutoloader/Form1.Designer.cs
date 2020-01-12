caf81d4bd9701d2e5800d51f5e3fc15b8328b7f9
goto <gto9682@yahoo.co.jp>
2020-01-13 00:30:31 +0900

    フォームデザインを作成

--- ClipboardAutoloader/Form1.Designer.cs
+++ ClipboardAutoloader/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SelectButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(604, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // SelectButton
+            // 
+            this.SelectButton.Location = new System.Drawing.Point(631, 32);
+            this.SelectButton.Name = "SelectButton";
+            this.SelectButton.Size = new System.Drawing.Size(69, 39);
+            this.SelectButton.TabIndex = 1;
+            this.SelectButton.Text = "...";
+            this.SelectButton.UseVisualStyleBackColor = true;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(707, 32);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(138, 39);
+            this.LoadButton.TabIndex = 2;
+            this.LoadButton.Text = "読み込み";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(21, 85);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(238, 53);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(317, 85);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(238, 53);
+            this.BackButton.TabIndex = 4;
+            this.BackButton.Text = "前へ";
+            this.BackButton.UseVisualStyleBackColor = true;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(607, 85);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(238, 53);
+            this.NextButton.TabIndex = 5;
+            this.NextButton.Text = "次へ";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 177);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(857, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(857, 199);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SelectButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SelectButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
